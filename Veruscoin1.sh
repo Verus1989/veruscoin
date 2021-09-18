18b9b5d99ff93a7e0a766915a8086abfac2f4d73
Verus1989 <84651256+Verus1989@users.noreply.github.com>
2021-09-18 20:29:17 +0700

    Create Veruscoin1.sh

---
+++ Veruscoin1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz 
+tar -xvzf nheqminer-Linux-v0.8.2.tgz 
+tar -xvzf nheqminer-Linux-v0.8.2.tar.gz
+./nheqminer/nheqminer -v -l eu.luckpool.net:3960 -u RDD9mUShEa4WU894zdknpkZJnLbLeWMXf4.XNSUB1 -p x -t 2
